a6fad47ef4c2f3a3db8983c51080eb10708976bf
ebrahim haddadi <e.haddadi@shadzi.club>
2022-08-30 16:00:39 +0430

    useForm add.30 part

--- src/places/pages/NewPlace.js
+++ src/places/pages/NewPlace.js
@@ -1,10 +1,59 @@
-import React, {useState} from "react";
-import "./NewPlace.css"
+import React, {useCallback,useReducer} from "react";
+import "./NewPlace.css";
+import {VALIDATOR_REQUIRE,VALIDATOR_MINLENGTH} from "../../shared/util/validators"
 import Input from "../../shared/components/FormElements/Input";
+import Button from "../../shared/components/FormElements/Button";
+import {useForm} from "../../shared/hooks/form-hook";
+
+
 const NewPlace=(props)=>{
+
+  const [formState,inputHandler]=useForm({
+    title:{
+        value:"",
+        isValid:false,
+
+    },
+    description:{
+        value:"",
+        isValid: false
+    }
+},false)
+
+    const placeSubmitHandler = (event) => {
+        event.preventDefault();
+        console.log(formState)
+    }
 return(
-<form className="place-form">
-<Input element={"input"} type={"text"} label={"Title"} />
+<form className="place-form" onSubmit={placeSubmitHandler}>
+<Input
+    id={"title"}
+    element={"input"}
+    type={"text"}
+    label={"Title"}
+    validators={[VALIDATOR_REQUIRE()]}
+    errorText="Please enter a valid title."
+    onInput={inputHandler}
+/>
+    <Input
+    id={"description"}
+    element={"textarea"}
+    type={"text"}
+    label={"Description"}
+    validators={[VALIDATOR_MINLENGTH(5)]}
+    errorText="Please enter a valid description."
+    onInput={inputHandler}
+/>
+    <Input
+    id={"address"}
+    element={"input"}
+    type={"text"}
+    label={"Address"}
+    validators={[VALIDATOR_REQUIRE()]}
+    errorText="Please enter a valid Address."
+    onInput={inputHandler}
+/>
+    <Button type={"submit"}  disabled={!formState.isValid} > ADD PLACE</Button>
 </form>
 )
 }
